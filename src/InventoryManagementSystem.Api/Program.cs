4abb8cee845db7c317d0842497eb3a1f91733483
rohannstu <rohansinha141635@gmail.com>
2026-08-02 17:07:29 +0600

    feat(infrastructure): add ef core dbcontext with category and product mappings

--- src/InventoryManagementSystem.Api/Program.cs
+++ src/InventoryManagementSystem.Api/Program.cs
@@ -1,7 +1,12 @@
+using InventoryManagementSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// ... existing builder setup ...
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
