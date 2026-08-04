d9eb8583bb0b9973d908855ec1877f8ae490810c
rohannstu <rohansinha141635@gmail.com>
2026-08-04 10:24:41 +0600

    feat(api+application): add product create and get endpoints with validation

---
+++ src/InventoryManagementSystem.Application/Products/Queries/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,5 @@
+using InventoryManagementSystem.Application.Abstractions.Messaging;
+
+namespace InventoryManagementSystem.Application.Products.Queries.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<ProductResponse?>;
